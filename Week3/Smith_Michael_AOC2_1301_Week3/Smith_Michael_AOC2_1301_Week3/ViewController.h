3b564117320490efae51cf9b5082d69d58daa7c6
mikesmithjr <mikesmithjr@fullsail.edu>
2013-01-21 22:44:54 -0600

    Setting Up Week 3 and adding some items to the Interface Builder

---
+++ Week3/Smith_Michael_AOC2_1301_Week3/Smith_Michael_AOC2_1301_Week3/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Smith_Michael_AOC2_1301_Week3
+//
+//  Created by Michael Smith on 1/21/13.
+//  Copyright (c) 2013 Michael Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
