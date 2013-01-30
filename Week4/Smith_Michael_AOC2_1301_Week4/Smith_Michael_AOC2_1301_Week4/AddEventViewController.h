10ad65d1dd109f84e4adf2265b8bf764738701aa
mikesmithjr <mikesmithjr@fullsail.edu>
2013-01-29 23:29:41 -0600

    Initial Commit for Week 4 Project

---
+++ Week4/Smith_Michael_AOC2_1301_Week4/Smith_Michael_AOC2_1301_Week4/AddEventViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddEventViewController.h
+//  Smith_Michael_AOC2_1301_Week4
+//
+//  Created by Michael Smith on 1/29/13.
+//  Copyright (c) 2013 Michael Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddEventViewController : UIViewController
+
+@end
---
+++ Week4/Smith_Michael_AOC2_1301_Week4/Smith_Michael_AOC2_1301_Week4/AddEventViewController.m
@@ -0,0 +1,38 @@
+//
+//  AddEventViewController.m
+//  Smith_Michael_AOC2_1301_Week4
+//
+//  Created by Michael Smith on 1/29/13.
+//  Copyright (c) 2013 Michael Smith. All rights reserved.
+//
+
+#import "AddEventViewController.h"
+
+@interface AddEventViewController ()
+
+@end
+
+@implementation AddEventViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
